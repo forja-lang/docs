31a79c74f139c1613d604b9d9053bd7f2fda6fdc
lococoi <lococoi@proton.me>
2026-07-10 04:55:55 -0300

    ci docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://forja-lang.github.io',
-  base: '/',
+  base: '/docs',
   outDir: './dist',
   srcDir: './src',
   publicDir: './public'
